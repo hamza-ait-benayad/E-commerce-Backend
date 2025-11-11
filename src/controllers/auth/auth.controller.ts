c7e1705c4eb7eb29cbfa51cb93fb3cfc1254736f
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2025-11-11 22:24:16 +0100

    add authentication controller

---
+++ src/controllers/auth/auth.controller.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
+import { loginService } from '../../services/auth/login';
+import { registerService } from '../../services/auth/register';
+import { ApiError } from '../../utils/apiError';
+
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw ApiError.badRequest(errors.array()[0].msg);
+    }
+    const result = await loginService(req.body);
+    res.status(200).json({
+      success: true,
+      message: 'Login successfully',
+      data: result,
+    });
+  } catch (error: any) {
+    next(error instanceof ApiError ? error : ApiError.internal(error.message));
+  }
+};
+
+export const register = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw ApiError.badRequest(errors.array()[0].msg);
+    }
+    const result = await registerService(req.body);
+    res.status(201).json({
+      success: true,
+      message: 'User registered successfully',
+      data: result,
+    });
+  } catch (error: any) {
+    next(error instanceof ApiError ? error : ApiError.internal(error.message));
+  }
+};
