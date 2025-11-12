aa5b04b45a778f8536a947459782f41d3e367469
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2025-11-12 23:35:34 +0100

    restructure auth service

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,9 +1,9 @@
-import { ApiError } from '../../utils/apiError';
-import prisma from '../../utils/prisma';
+import { ApiError } from "../utils/apiError";
+import prisma from "../utils/prisma";
 import bcrypt from 'bcrypt';
-import { env } from "../../config/env";
 import jwt from "jsonwebtoken";
-import { logger } from '../../utils/logger';
+import { env } from "../config/env";
+import { logger } from "../utils/logger";
 import { Prisma } from '@prisma/client';
 
 export interface RegisterRequestBody {
@@ -12,9 +12,81 @@ export interface RegisterRequestBody {
   name: string;
 }
 
-export async function registerService(body: RegisterRequestBody) {
+export interface LoginRequestBody {
+  email: string,
+  password: string
+}
+
+
+export const loginService = async (body: LoginRequestBody) => {
+
+  const { email, password } = body;
+
+  const user = await prisma.user.findUnique({
+    where: { email },
+    select: {
+      id: true,
+      email: true,
+      password: true,
+      name: true,
+      role: true,
+      emailVerified: true,
+      isActive: true,
+      createdAt: true,
+      updatedAt: true
+    }
+  });
+
+  if (!user) {
+    throw ApiError.notFound('invalid email or password');
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    throw ApiError.unauthorized('Invalid email or password');
+  }
+
+  const result = await prisma.$transaction(async (tx) => {
+    const accessToken = jwt.sign(
+      { userId: user.id, email: user.email, role: user.role },
+      env.JWT_ACCESS_SECRET,
+      { expiresIn: env.JWT_ACCESS_EXPIRY } as jwt.SignOptions
+    );
+
+    const refreshToken = jwt.sign(
+      { userId: user.id },
+      env.JWT_REFRESH_SECRET,
+      { expiresIn: env.JWT_REFRESH_EXPIRY } as jwt.SignOptions
+    );
+
+    const expiresAt = new Date();
+    expiresAt.setDate(expiresAt.getDate() + 7);
+
+    await tx.refreshToken.create({
+      data: {
+        token: refreshToken,
+        userId: user.id,
+        expiresAt: expiresAt
+      }
+    });
+    return { accessToken, refreshToken };
+  });
+
+  const { password: _, ...userWithoutPassword } = user;
+
+
+  return {
+    user: userWithoutPassword,
+    accessToken: result.accessToken,
+    refreshToken: result.refreshToken
+  };
+}
+
+
+export const registerService = async (body: RegisterRequestBody) => {
   const { email, password, name } = body;
-  // The controller must validate required fields and uniqueness, do not duplicate here.
+
   const hashedPassword = await bcrypt.hash(password, env.BCRYPT_ROUNDS);
   try {
     const result = await prisma.$transaction(async (tx) => {
