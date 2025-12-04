cdd3df7bc71ac140eb03a6a091bec97903c9a3a8
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2025-12-04 21:13:48 +0100

    product routers update

--- src/routes/v1/product.routes.ts
+++ src/routes/v1/product.routes.ts
@@ -1,14 +1,15 @@
 import express from "express"
+import { createProduct, deleteProduct, getAllProducts, getProductById, getProductBySlug } from "../../controllers/product.controller";
+import { body } from "express-validator";
 
 
 const router = express.Router();
 
-
 router.get('/', getAllProducts);
 router.get('/:id', getProductById);
-
+router.get('/slug/:slug', getProductBySlug);
 router.post('/', createProduct);
-router.put('/:id', updateProduct);
+// router.put('/:id', validateProduct, updateProduct);
 router.delete('/:id', deleteProduct);
 
 export default router;
